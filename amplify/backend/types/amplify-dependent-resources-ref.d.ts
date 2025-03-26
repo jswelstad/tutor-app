f1ee5fbb451e3384b25ecc1327f2a4229384a2a9
kcverbrugge <kverbruggestudy@gmail.com>
2025-03-26 14:21:19 -0600

    Ran amplify pull <appID>

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "tutorappf3be7136": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
